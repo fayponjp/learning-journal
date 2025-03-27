c89e78d06eb6475ef47bef4d3e2910ac73b8248a
fayponjp <fayponjp@gmail.com>
2025-03-27 15:33:55 +0800

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,10 @@
+[
+    {
+        id: 1,
+        title: "Blog one",
+        date: "July 23, 2022",
+        blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        full: "",
+        flavorTitle: "My new journey as a bootcamp student."
+    }
+]
