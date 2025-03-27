b5452b3581143186034e34c6a9f8df9ccb406a58
fayponjp <fayponjp@gmail.com>
2025-03-27 15:34:25 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,5 +6,13 @@
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         full: "",
         flavorTitle: "My new journey as a bootcamp student."
-    }
+    },
+    {
+        id: 2,
+        title: "Blog two",
+        date: "July 23, 2022",
+        blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        full: "",
+        flavorTitle: "My new journey as a bootcamp student."
+    },
 ]
